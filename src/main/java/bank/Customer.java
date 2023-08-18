add5ec1a6cfd5d483c34d3c2790920114f383003
thausyn <142385415+thausyn@users.noreply.github.com>
2023-08-18 02:57:54 +0000

    create authenticator class

--- src/main/java/bank/Customer.java
+++ src/main/java/bank/Customer.java
@@ -6,6 +6,7 @@ public class Customer {
   private String username;
   private String password;
   private int account_id;
+  private boolean authenticated;
 
   public Customer(int id, String name, String username, String password, int account_id) {
     this.id = id;
@@ -13,6 +14,7 @@ public class Customer {
     this.username = username;
     this.password = password;
     this.account_id = account_id;
+    this.authenticated = false;
   }
 
   public int getId() {
@@ -53,6 +55,14 @@ public class Customer {
 
   public void setAccount_id(int account_id) {
     this.account_id = account_id;
+  }
+
+  public boolean isAuthenticated() {
+    return this.authenticated;
+  }
+
+  public void setAuthenticated(boolean authenticated) {
+    this.authenticated = authenticated;
   }
 
 }
